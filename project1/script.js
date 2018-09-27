63543490be676d749eeb4e8f34ae6b95dc21717b
Christopher Siegfried <35548245+csiegfri@users.noreply.github.com>
2018-09-26 22:30:24 -0500

    changed back to alert

--- project1/script.js
+++ project1/script.js
@@ -2,6 +2,6 @@ var app = angular.module('myApp', []);
 app.controller('MainCtrl', function($scope) 
 {
     $scope.register=function(uname, email){
-        $scope.confirm = "Congratulations, " + uname + ", you're in. Expect an email at " + email + " in the near future.";
+        alert("Congratulations, " + uname + ", you're in. Expect an email at " + email + " in the near future.");
     }
 });
