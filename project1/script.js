fe318f900253caf0d1f56025f27af50ae4cb62a9
Christopher Siegfried <35548245+csiegfri@users.noreply.github.com>
2018-09-26 21:51:05 -0500

    added angular function

--- project1/script.js
+++ project1/script.js
@@ -1,5 +1,10 @@
 var app = angular.module('myApp', []);
 app.controller('MainCtrl', function($scope) 
 {
-    
+    name = $scope.uname;
+    mail = $scope.email;
+
+    $scope.register=function(name, mail){
+        alert("Congratulations, " + name + ", you're in. Expect an email at " + mail + " in the future.")
+    }
 });
